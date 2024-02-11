334198cd3fb53e3bb42c06602cdb78422110bf57
Anosike Osifo <code.anosikeosifo@gmail.com>
2024-02-12 00:39:31 +0100

    5.4 -  light it up

--- led-roulette/src/main.rs
+++ led-roulette/src/main.rs
@@ -4,13 +4,18 @@
 
 use cortex_m_rt::entry;
 use panic_halt as _;
-use microbit as _;
+use microbit::board::Board;
+use microbit::hal::prelude::*;
 
 #[entry]
-fn main() -> ! { // the exclamation return type indicates that this method does not return (it  runs infinitely)
-    let _y;
-    let x = 42;
-    _y = x;
+fn main() -> ! { 
+    let mut board = Board::take().unwrap();
+
+    board.display_pins.col1.set_low().unwrap();
+    board.display_pins.row1.set_high().unwrap();
+
+    board.display_pins.col3.set_low().unwrap();
+    board.display_pins.row3.set_high().unwrap();
 
     loop {} //infinite loop to keep the function running
 }
