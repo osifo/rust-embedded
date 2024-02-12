dbe05a8d965680b670990a300ae799eaf968c282
Anosike Osifo <code.anosikeosifo@gmail.com>
2024-02-12 01:30:22 +0100

    5.5 - it blinks

--- led-roulette/src/main.rs
+++ led-roulette/src/main.rs
@@ -3,19 +3,40 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
+use rtt_target::{rtt_init_print, rprintln};
+use panic_rtt_target as _;
 use microbit::board::Board;
+use microbit::hal::timer::Timer;
 use microbit::hal::prelude::*;
 
 #[entry]
 fn main() -> ! { 
+    rtt_init_print!();
+
     let mut board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
 
-    board.display_pins.col1.set_low().unwrap();
-    board.display_pins.row1.set_high().unwrap();
+    let mut col1 = board.display_pins.col1;
+    let mut col2 = board.display_pins.col2;
+    let mut row1 = board.display_pins.row1;
+    let mut row2 = board.display_pins.row2;
 
-    board.display_pins.col3.set_low().unwrap();
-    board.display_pins.row3.set_high().unwrap();
+    loop {
+        row1.set_high().unwrap();
+        col1.set_high().unwrap();
+        col2.set_low().unwrap();
+        row2.set_low().unwrap();
+        rprintln!("dark, light");
 
-    loop {} //infinite loop to keep the function running
+        timer.delay_ms(1000u16);
+
+        row2.set_high().unwrap();
+        col1.set_low().unwrap();
+
+        row1.set_low().unwrap();
+        col2.set_high().unwrap();
+        rprintln!("light, dark");
+
+        timer.delay_ms(1000u16);
+    }
 }
